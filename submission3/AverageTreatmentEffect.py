afdb8cd4294805b1b6437e3e7b6a4d009991c1e2
Baran <baran.pasa@emory.edu>
2025-02-19 17:04:03 -0500

    Update AverageTreatmentEffect.py

--- submission3/AverageTreatmentEffect.py
+++ submission3/AverageTreatmentEffect.py
@@ -7,3 +7,6 @@ import seaborn as sns
 HCRIS_data = pd.read_csv('submission3/data/output/HCRIS_Data.csv')
 HCRIS_data = HCRIS_data[HCRIS_data['year'] == 2012]
 
+print(HCRIS_data.columns)
+
+HCRIS_data.groupby(['provider_number'])
